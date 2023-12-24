8829e2075178b03a80c2ef57b4b44103f51474d9
Mykyta Burkovskyi <burkovskyi.work@gmail.com>
2023-12-25 01:59:42 +0200

    Disable debug logs on prod

--- config/prod.exs
+++ config/prod.exs
@@ -3,3 +3,5 @@ import Config
 config :angler,
   host: "#{System.fetch_env!("APP_NAME")}.gigalixirapp.com",
   local_port: System.get_env("PORT", "4000") |> String.to_integer()
+
+config :logger, level: :info
