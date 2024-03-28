2b71cb118b5093ab628d05b05f2da7c68fe54761
Mykyta Burkovskyi <burkovskyi.work@gmail.com>
2024-03-28 13:17:22 +0200

    Fix startup

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,4 +4,4 @@ config :angler,
   token: System.fetch_env!("BOT_TOKEN"),
   max_bot_concurrency: System.get_env("BOT_MAX_CONCURRENTCY", "1000") |> String.to_integer(),
   host: "#{System.fetch_env!("APP_NAME")}.gigalixirapp.com",
-  local_port: System.get_env("PORT", "4000") |> String.to_integer(),
+  local_port: System.get_env("PORT", "4000") |> String.to_integer()
